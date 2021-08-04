1e49345ba3464d30878802ae2af17ab6d7bbb7a2
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-04 19:16:59 -0300

    Adiciona comentários

--- src/zoo.js
+++ src/zoo.js
@@ -1,31 +1,37 @@
 const { species, employees, prices } = require('./data');
 const data = require('./data');
 
+// -----1-----
 function getSpeciesByIds(...ids) {
   return species.filter(({ id }) => ids.includes(id));
 }
 
+// -----2-----
 function getAnimalsOlderThan(animal, agePopulation) {
   const findAnimal = species.find(({ name }) => animal.includes(name));
   return findAnimal.residents.every(({ age }) => age >= agePopulation);
 }
 
+// -----3-----
 function getEmployeeByName(employeeName) {
   if (typeof employeeName === 'undefined') return {};
   return employees.find((element) => element.firstName
   === employeeName || element.lastName === employeeName);
 }
 
+// -----4-----
 function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
 
+// -----5-----
 function isManager(id) {
   // Colocar todos os gerentes em um Array e verificar se o id está no array.
   const managersList = employees.map((element) => element.managers);
   return managersList.some((element) => element.includes(id));
 }
 
+// -----6-----
 function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
   const newObject = {
     id,
@@ -37,6 +43,7 @@ function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []
   return employees.push(newObject);
 }
 
+// -----7-----
 function countAnimals(specieName) {
   // Utilizar uma HOF para procurar a espécie e retornar o número da população.
   if (typeof specieName === 'undefined') {
@@ -48,16 +55,19 @@ function countAnimals(specieName) {
   return species.find(({ name }) => name === specieName).residents.length;
 }
 
+// -----8-----
 function calculateEntry(entrants) {
   if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
   const { Adult = 0, Senior = 0, Child = 0 } = entrants;
   return (prices.Adult * Adult) + (prices.Senior * Senior) + (prices.Child * Child);
 }
 
+// -----9-----
 function getAnimalMap(options) {
   // seu código aqui
 }
 
+// -----10-----
 function getSchedule(dayName) {
   const daysObject = {
     Tuesday: 'Open from 8am until 6pm',
@@ -74,6 +84,7 @@ function getSchedule(dayName) {
   return { [`${dayName}`]: daysObject[`${dayName}`] };
 }
 
+// -----11-----
 function getOldestFromFirstSpecies(ID) {
   const findId = employees.find(({ id }) => id === ID);
   const findAnimalEmployee = findId.responsibleFor[0];
@@ -82,6 +93,7 @@ function getOldestFromFirstSpecies(ID) {
   return Object.values(oldestAnimal);
 }
 
+// -----12-----
 function increasePrices(percentage) {
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round
   const profit = percentage / 100;
@@ -94,6 +106,7 @@ function increasePrices(percentage) {
   prices.Child = Math.round(childProfit * 100) / 100;
 }
 
+// -----13-----
 function getEmployeeCoverage(idOrName) {
   const getEmployee = employees.find(({ id, firstName, lastName }) => id === idOrName
    || firstName === idOrName || lastName === idOrName);
