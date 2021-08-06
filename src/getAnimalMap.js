e0aa97015533b30b118b8bd845f5d5d3cd2928d5
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-06 13:59:40 -0300

    Req 9: Parte I quase completa

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -19,6 +19,22 @@ function noParameter(options) {
   }
 }
 
+const animalsResidents = (animal) => {
+  const residentsFilter = species.find((element) => element.name === animal).residents;
+  return residentsFilter.map((element) => element.name);
+};
+
+const test = (region) => {
+  const ne = animalsRegion(region);
+  return ne.reduce((acc, curr) => {
+    const animalsObject = { [curr]: animalsResidents(curr) };
+    acc.push(animalsObject);
+    return acc;
+  }, []);
+};
+console.log(test('NE'));
+
 module.exports = {
   noParameter,
+  animalsResidents,
 };
